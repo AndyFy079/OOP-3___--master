dc49b7a3503783a0d6cf6f8dc1f2d7fba31fb919
Дима Звягин <@home.local>
2020-03-16 13:35:48 +0200

    Добавьте файлы проекта.

---
+++ OOP(3___)/Derived.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_3____
+{
+    class Derived : Found
+    {
+        public Derived() { }
+        public Derived(string name, int cred, int deb) : base(name, cred) { debet = deb; }
+        protected int debet;
+        public void DerivedMethod() { Console.WriteLine("Это метод класса Derived"); }
+        new public void Analysis() { base.Analysis(); Console.WriteLine("Сложный анализ"); }
+        public void Analysis(int level)
+        { base.Analysis(); Console.WriteLine("Анализ глубины {0}", level); }
+        public override void VirtMethod() { Console.WriteLine("Сын: " + this.ToString()); }
+        public override string ToString() { return (String.Format("поля: name = {0}, credit = {1},debet ={2}", name, credit, debet)); }
+        public void Work()
+        {
+            VirtMethod();
+            NonVirtMethod();
+            Analysis();
+        }
+    }
+}
---
+++ OOP(3___)/Found.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_3____
+{
+    class Found
+    {
+        protected string name;
+        protected int credit;
+        public Found() { }
+        public Found(string name, int sum) { this.name = name; credit = sum; }
+        public virtual void VirtMethod() { Console.WriteLine("Отец: " + this.ToString()); }
+        public override string ToString() { return (String.Format("поля: name = {0}, credit = {1}", name, credit)); }
+        public void NonVirtMethod() { Console.WriteLine("Мать: " + this.ToString()); }
+        public void Analysis() { Console.WriteLine("Простой анализ"); }
+        public void Work() { VirtMethod(); NonVirtMethod(); Analysis(); }
+    }
+}
